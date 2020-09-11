42bbe4e08f49df48b9d80abdb95889ed42766674
Noel Garces <noel.garces08@gmail.com>
2020-09-10 19:57:10 -0600

    Add css reset stylesheet

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/main-layout');
 app.use(expressLayouts);
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
     res.render('blog');
