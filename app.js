af255f075c769c622bbc41324ba6376fb49303da
Noel Garces <noel.garces08@gmail.com>
2020-09-10 18:26:49 -0600

    Established the main layout and the admin layout

--- app.js
+++ app.js
@@ -6,12 +6,16 @@ const PORT = 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
-app.set('layout', 'layouts/layout');
+app.set('layout', 'layouts/main-layout');
 app.use(expressLayouts);
 
 app.get('/', function(req, res) {
-    res.render('blog/index');
+    res.render('blog');
 })
+
+app.get('/admin', function(req, res) {
+    res.render('admin', {layout: './layouts/admin-layout'})
+});
 
 app.listen(PORT, function() {
     console.log(`Server is running on http://localhost:${PORT}`)
