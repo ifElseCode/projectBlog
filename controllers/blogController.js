d01c746175f09bbf15c73c48b336b1d0f8b6574c
Vitoria Santos <66972502+vitoriasantospe@users.noreply.github.com>
2020-09-28 23:22:19 -0700

    tried to add routes for individual blog posts

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,5 +1,13 @@
-blogs_get = (req, res) => res.render("blog/blogs");
+const blog = require("../models/blog");
+
+const blogs_get = (req, res) => res.render("blog/blogs");
+
+const blogs_single_get = (req, res) => {
+    res.render("blog/clicked.ejs");
+}
+
 
 module.exports = {
-  blogs_get
+  blogs_get,
+  blogs_single_get
 }
--- routes/blogRouter.js
+++ routes/blogRouter.js
@@ -3,5 +3,7 @@ const blogRouter = express.Router();
 const blogController = require("../controllers/blogController");
 
 blogRouter.get("/", blogController.blogs_get);
+blogRouter.get("/blog/clicked", blogController.blogs_single_get);
+
 
 module.exports = blogRouter;
