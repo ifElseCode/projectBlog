3801be8c4717aa13f346670ec1831547c0929e4d
Vitoria Santos <66972502+vitoriasantospe@users.noreply.github.com>
2020-09-30 11:27:13 -0700

    Add routes for home page and all blogs pages

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,9 +1,23 @@
-const blog = require("../models/blog");
+const Blog = require("../models/blog");
 
-const blogs_get = (req, res) => res.render("blog/blogs");
+const blogs_get = async (req, res) => {
+  try {
+    const blogs = await Blog.find();
+    res.render("blog/blogs.ejs", {blogs});
+  }
+  catch (err) {
+    console.log(err);
+  }
+}
 
-const blogs_single_get = (req, res) => {
-    res.render("blog/clicked.ejs");
+const blogs_single_get = async (req, res) => {
+    try {
+      const blogs = await Blog.findById(id);
+      res.render("blog/blog.ejs", {blogs});
+    }
+    catch (err) {
+      console.log(err);
+    }
 }
 
 
