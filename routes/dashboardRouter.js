5c410fe1ac4d44a7e52ccaa034d1abed9c8d01f9
Stephen Scholtz <scholtz.gnome@gmail.com>
2020-09-30 12:30:10 +0200

    made some small edits to route and controller names, and reordered the items in the dashboard nav-bar

--- routes/dashboardRouter.js
+++ routes/dashboardRouter.js
@@ -3,10 +3,10 @@ const dashboardRouter = express.Router();
 const dashboardController = require("../controllers/dashboardController");
 
 dashboardRouter.get("/", dashboardController.dashboard_get);
-dashboardRouter.get("/user-roles", dashboardController.dashboard_user_roles_get);
-dashboardRouter.delete("/user-roles/:id", dashboardController.dashboard_user_roles_delete);
-dashboardRouter.patch("/user-roles/admin/:id", dashboardController.dashboard_user_roles_admin_patch);
-dashboardRouter.patch("/user-roles/author/:id", dashboardController.dashboard_user_roles_author_patch);
+dashboardRouter.get("/user-roles", dashboardController.dashboard_users_get);
+dashboardRouter.delete("/users/:id", dashboardController.dashboard_users_delete);
+dashboardRouter.patch("/users/admin/:id", dashboardController.dashboard_users_admin_patch);
+dashboardRouter.patch("/users/author/:id", dashboardController.dashboard_users_author_patch);
 dashboardRouter.get("/blog-posts", dashboardController.dashboard_blog_posts_get);
 dashboardRouter.get("/blog-posts/create", dashboardController.dashboard_blog_posts_create_get);
 dashboardRouter.post("/blog-posts/create", dashboardController.dashboard_blog_posts_create_post);
