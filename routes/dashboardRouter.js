9d9833131688a54b561c951009fa733f3fadc61e
Stephen Scholtz <scholtz.gnome@gmail.com>
2020-10-01 11:16:48 +0200

    neatened up the names of some routes, controllers and the relevant files, as well as fixed the order of the blogs displayed in the dashboard

--- routes/dashboardRouter.js
+++ routes/dashboardRouter.js
@@ -7,9 +7,9 @@ dashboardRouter.get("/users", dashboardController.dashboard_users_get);
 dashboardRouter.delete("/users/:id", dashboardController.dashboard_users_delete);
 dashboardRouter.patch("/users/admin/:id", dashboardController.dashboard_users_admin_patch);
 dashboardRouter.patch("/users/author/:id", dashboardController.dashboard_users_author_patch);
-dashboardRouter.get("/blog-posts", dashboardController.dashboard_blog_posts_get);
-dashboardRouter.get("/blog-posts/create", dashboardController.dashboard_blog_posts_create_get);
-dashboardRouter.post("/blog-posts/create", dashboardController.dashboard_blog_posts_create_post); 
-dashboardRouter.delete("/blog-posts/:id", dashboardController.dashboard_blog_posts_delete); 
+dashboardRouter.get("/blogs", dashboardController.dashboard_blogs_get);
+dashboardRouter.get("/blogs/create", dashboardController.dashboard_blogs_create_get);
+dashboardRouter.post("/blogs/create", dashboardController.dashboard_blogs_create_post); 
+dashboardRouter.delete("/blogs/:id", dashboardController.dashboard_blogs_delete); 
 
 module.exports = dashboardRouter;
