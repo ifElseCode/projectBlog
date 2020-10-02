ffa318a473a854f6451630cf9574ec0df13eba5b
Stephen Scholtz <scholtz.gnome@gmail.com>
2020-10-02 11:18:09 +0200

    displayed blogs data from server in the same format as placeholder html

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,9 +1,10 @@
 const Blog = require("../models/blog");
 
 const blogs_get = async (req, res) => {
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   try {
-    const blogs = await Blog.find();
-    res.render("blog/blogs.ejs", {blogs});
+    const blogs = await Blog.find().sort({ date: -1 });
+    res.render("blog/blogs.ejs", { blogs, months });
   }
   catch (err) {
     console.log(err);
@@ -11,15 +12,14 @@ const blogs_get = async (req, res) => {
 }
 
 const blogs_single_get = async (req, res) => {
-    try {
-      const blogs = await Blog.findById(id);
-      res.render("blog/blog.ejs", {blogs});
-    }
-    catch (err) {
-      console.log(err);
-    }
+  try {
+    const blogs = await Blog.findById(id);
+    res.render("blog/blog.ejs", {blogs});
+  }
+  catch (err) {
+    console.log(err);
+  }
 }
-
 
 module.exports = {
   blogs_get,
